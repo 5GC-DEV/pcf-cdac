d941a23c90afbcfbb674a23783d6e5d7d2172820
anaswara <anaswara.n@cdac.in>
2025-02-14 16:06:45 +0530

    structure updated

--- context/context.go
+++ context/context.go
@@ -75,6 +75,9 @@ type PCFContext struct {
 
 type SessionPolicy struct {
 	SessionRules           map[string]*models.SessionRule
+	PccRules               map[string]*models.PccRule
+	QosDecs                map[string]*models.QosData
+	TraffContDecs          map[string]*models.TrafficControlData
 	SessionRuleIdGenerator *idgenerator.IDGenerator
 }
 
