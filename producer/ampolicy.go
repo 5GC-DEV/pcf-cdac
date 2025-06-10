5a2a8396430920db21e463c70427efcd3c30cb79
ReshmaJS <reshma.js@cdac.in>
2025-06-10 15:51:06 +0530

    add logs to ampolicy post data

--- producer/ampolicy.go
+++ producer/ampolicy.go
@@ -260,7 +260,7 @@ func PostPoliciesProcedure(polAssoId string,
 			amPolicy = ue.NewUeAMPolicyData(assolId, policyAssociationRequest)
 		}
 		amPolicy.AmPolicyData = &amData
-		for _, amPolicy := range amPolicy.AMPolicyData {
+		for _, amPolicy := range amPolicy.AmPolicyData {
 			logger.AMpolicylog.Infof("AMPolicy create access type[%s] from ampolicy", amPolicy.AccessType)
 			logger.AMpolicylog.Infof("AMPolicy create plmn [%s]  from ampolicy", amPolicy.ServingPlmn)
 		}
