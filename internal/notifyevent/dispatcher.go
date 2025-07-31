598b562974f10b21e2700958bc3370fe7ce170f8
anaswara <anaswara.n@cdac.in>
2025-07-31 15:52:14 +0530

    buffer set to 0

--- internal/notifyevent/dispatcher.go
+++ internal/notifyevent/dispatcher.go
@@ -15,7 +15,7 @@ var notifyDispatcher *event.Dispatcher
 
 func RegisterNotifyDispatcher() error {
 	logger.NotifyEventLog.Infof("RegisterNotifyDispatcher() called")
-	notifyDispatcher = event.NewDispatcher(5, 1)
+	notifyDispatcher = event.NewDispatcher(5, 0)
 	if err := notifyDispatcher.Register(NotifyListener{},
 		SendSMpolicyUpdateNotifyEventName,
 		SendSMpolicyTerminationNotifyEventName); err != nil {
